eb7feda3486d117c3b2e94542cc4aebb16bf569b
Александр Хасанов <AlexanderKhasanov1205@yandex.ru>
2023-07-25 17:16:42 +0300

    new popup

--- pages/script.js
+++ pages/script.js
@@ -36,13 +36,108 @@ const initialCards = [
   }
 ];
 
-function setUserInfo() {
-  formProfileInfo.username.value = profile.querySelector('.profile__name').textContent.trim();
-  formProfileInfo.user_info.value = profile.querySelector('.profile__about').textContent.trim();
+class Form {
+  constructor (name) {
+    this.name = name;
+    this.templateForm = document.createElement('form');
+    this.templateForm.classList.add('form');
+    this.templateForm.name = name;
+    console.log(this.templateForm);
+  }
+
+  createTitle(title) {
+    const titleElement = document.createElement('h2');
+    titleElement.classList.add('form__title');
+    titleElement.textContent = title;
+    this.templateForm.append(titleElement);
+  }
+
+  createInput(names, placeholders){
+    if (!Array.isArray(names) && !Array.isArray(placeholders)){
+      console.log('Метод принимает на вход массивы names, placeholders');
+      return
+    }
+    if (names.length != placeholders.length){
+      console.log('Массивы names, placeholders должны быть одинаковой длины');
+      return;
+    }
+
+    const fieldSetElement = document.createElement('fieldset')
+    fieldSetElement.classList.add('form__input-container');
+
+    names.forEach((name, index) => {
+      const inputElement = document.createElement('input');
+      inputElement.classList.add('form__item')
+      inputElement.type = 'text';
+      inputElement.name = name;
+      inputElement.placeholders = placeholders[index];
+      fieldSetElement.append(inputElement);
+    });
+
+    this.templateForm.append(fieldSetElement);
+  }
+
+  createButton(name, text){
+    const buttonElement = document.createElement('button');
+    buttonElement.classList.add('form__send-btn');
+    buttonElement.type = 'submit';
+    buttonElement.name = name;
+    buttonElement.value = `${this.name}_${name}`;
+    buttonElement.textContent = text;
+
+    this.templateForm.append(buttonElement);
+  }
 }
 
-function showPopup() {
-  setUserInfo();
+//const formTemplate = page.querySelector('#form-template').content;
+
+function setUserInfo(form) {
+  console.log('setUserInfo');
+  form.username.value = profile.querySelector('.profile__name').textContent.trim();
+  form.user_info.value = profile.querySelector('.profile__about').textContent.trim();
+}
+
+function getFromInput(){}
+
+function getProfileForm() {
+  const profileForm = new Form('profile');
+
+  profileForm.createTitle('Редактировать профиль');
+  profileForm.createInput(
+    ['username', 'user_info'],
+    ['Ваше имя', 'Пару слов о себе']
+  );
+  profileForm.createButton('save', 'Сохранить');
+  return profileForm.templateForm;
+}
+
+function getPostForm() {
+}
+
+const profileForm = getProfileForm();
+
+//popup.querySelector('.popup__container').innerHTML = profileForm.outerHTML;
+
+function showPopup(evt) {
+  console.log(evt.target);
+
+  const popupContent = popup.querySelector('.popup__container .popup__content');
+  console.log(popupContent);
+  switch (evt.target.name){
+    case 'edit_profile':
+      console.log('edit_profile');
+      /*
+      const form = container.querySelector('.form');
+      if (form !== profileForm){
+        console.log('new form');
+        container.append(profileForm);
+        setUserInfo(form);
+      }*/
+      setUserInfo(profileForm);
+      popupContent.innerHTML = profileForm.outerHTML;
+      break;
+  }
+
   popup.classList.add('popup_opened');
   popupContainer.classList.add('popup__container_opened');
 }
@@ -73,9 +168,13 @@ function showPosts() {
 }
 
 showPosts();
+/*
+const t = page.querySelector('#form-template').content;
+const t_clone = t.querySelector('.form').cloneNode(true);
+console.log(t_clone);*/
 
 profileEditBtn.addEventListener('click', showPopup);
 popupCloseBtn.addEventListener('click', closePopup);
-formProfileInfo.addEventListener('submit', saveProfileInfo);
+//formProfileInfo.addEventListener('submit', saveProfileInfo);
 addPostBtn.addEventListener('click', showPopup);
 

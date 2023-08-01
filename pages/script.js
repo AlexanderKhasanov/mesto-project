6040b832090fc2a8161c19c79713f2d759038f31
Alexander Khasanov <AlexanderKhasanov1205@yandex.ru>
2023-08-01 10:34:59 +0300

    add focus on image

--- pages/script.js
+++ pages/script.js
@@ -1,3 +1,4 @@
+// Класс для создания формы
 class Form {
   constructor (name) {
     this.name = name;
@@ -67,6 +68,7 @@ const addPostBtn = profile.querySelector('.profile__add-btn');
 const profileForm = getProfileForm();
 const addPostForm = getPostForm();
 
+// Стартовые посты
 const initialCards = [
   {
     name: 'Архыз',
@@ -96,7 +98,7 @@ const initialCards = [
   createPost(post.name, post.link)
 });
 
-
+// Сохранение измененной информации о пользователе
 function saveProfileInfo(evt) {
   evt.preventDefault();
   profile.querySelector('.profile__name').textContent = profileForm.username.value;
@@ -104,12 +106,15 @@ function saveProfileInfo(evt) {
   closePopup();
 }
 
+// Добавления поста через форму
 function addPost(evt) {
   evt.preventDefault();
   createPost(addPostForm.post_name.value, addPostForm.post_url.value);
   closePopup();
 }
 
+
+// Создание формы для изменения информации о пользователе
 function getProfileForm() {
   const profileForm = new Form('profile');
 
@@ -122,6 +127,7 @@ function getProfileForm() {
   return profileForm.templateForm;
 }
 
+// Создание формы для добавления поста
 function getPostForm() {
   const addPostForm = new Form('add_post');
 
@@ -134,12 +140,17 @@ function getPostForm() {
   return addPostForm.templateForm;
 }
 
+// Заполнение формы для изменения информации о пользователе
+// текущей информацией
 function setUserInfo(form) {
   form.username.setAttribute('value', profile.querySelector('.profile__name').textContent.trim());
   form.user_info.value = profile.querySelector('.profile__about').textContent.trim();
 }
 
-function showPopup(evt) {
+// Открытие модального окна с формами
+function showInfoPopup(evt) {
+  popup.classList.remove('popup_type_focus');
+  popupContainer.classList.remove('popup__container_type_focus');
   const popupContent = popup.querySelector('.popup__container .popup__content');
   switch (evt.target.name){
     case 'edit_profile':
@@ -158,19 +169,65 @@ function showPopup(evt) {
   }
 
   popup.classList.add('popup_opened');
-  popupContainer.classList.add('popup__container_opened');
+  popupContainer.classList.add(
+    'popup__container_opened', 'popup__container_type_info'
+  );
 }
 
+// Закрытие модального окна
 function closePopup() {
   popup.classList.remove('popup_opened');
   popupContainer.classList.remove('popup__container_opened');
 }
 
+// Добавление увеличенного изображения в модальное окно
+function createFocusImage (img, caption) {
+  const imageContainer = document.createElement('figure');
+  imageContainer.classList.add('focus-img');
+
+  const image = document.createElement('img');
+  image.classList.add('focus-img__image');
+  image.src = img;
+
+  const imageCaption = document.createElement('figcaption');
+  imageCaption.classList.add('focus-img__caption');
+  imageCaption.textContent = caption.textContent;
+
+  imageContainer.append(image);
+  imageContainer.append(imageCaption);
+
+  return imageContainer;
+}
+
+// Открытие модального окна с увеличенным изображением
+function focusImagePopup(evt) {
+  popupContainer.classList.remove('popup__container_type_info');
+
+  const popupContent = popup.querySelector('.popup__container .popup__content');
+  popupContent.childNodes.forEach(item => item.remove());
+
+  const focusImage = createFocusImage(
+    evt.target.src,
+    evt.target.closest('.post').querySelector('.post__title')
+  );
+
+  popupContent.append(focusImage);
+
+  popup.classList.add(
+    'popup_opened', 'popup_type_focus'
+  );
+  popupContainer.classList.add(
+    'popup__container_opened', 'popup__container_type_focus'
+  );
+}
+
+// Создание поста
 function createPost(name, link) {
   const postElement = postTemplate.querySelector('.posts__item').cloneNode(true);
   const postImg = postElement.querySelector('.post__image');
   postImg.src = link;
   postImg.alt = `Фотография поста. ${link}`;
+  postImg.addEventListener('click', focusImagePopup);
   postElement.querySelector('.post__title').textContent = name;
   postElement.querySelector('.post__like').addEventListener(
     'click', (evt) => {evt.target.classList.toggle('post__like_active')}
@@ -181,12 +238,20 @@ function createPost(name, link) {
   postContainer.prepend(postElement);
 }
 
-initialCards
+// Открытие модальных окон
+profileEditBtn.addEventListener('click', showInfoPopup);
+addPostBtn.addEventListener('click', showInfoPopup);
 
-//showPosts();
-
-profileEditBtn.addEventListener('click', showPopup);
+// Закрытие модальных окон
+// По нажатию на кнопку закрытия
 popupCloseBtn.addEventListener('click', closePopup);
-//formProfileInfo.addEventListener('submit', saveProfileInfo);
-addPostBtn.addEventListener('click', showPopup);
-
+// По нажатию клавиши  Esc
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape')
+    closePopup();
+});
+// По нажатию на область вне модального окна
+popup.addEventListener('click', (evt) => {
+  if (evt.target === popup)
+    closePopup();
+});
